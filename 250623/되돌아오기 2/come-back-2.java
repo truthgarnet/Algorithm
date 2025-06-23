5e3c1b45413dd2a4b289e4df9bbb27e50238df80
Eunseo Kong <57488270+truthgarnet@users.noreply.github.com>
2025-06-23 17:10:32 +0900

    Title: 되돌아오기 2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250623/되돌아오기 2/come-back-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static final int[] dx = {1, -1, 0, 0};
+    public static final int[] dy = {0, 0, -1, 1};
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String commands = sc.next();
+
+        String[] s = commands.split("");
+        int dir = 3;
+        int x = 0, y = 0;
+        int cnt = 0;
+        boolean flag = false;
+        int turn = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("R")) {
+                turn++; // change direction
+                cnt++;
+                dir = (dir + turn) % 4;        
+            } else if (s[i].equals("F")) { // move
+                x = x + dx[dir];
+                y = y + dy[dir];
+                cnt++;
+                if (x == 0 && y == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        int result = flag ? cnt : -1;
+        System.out.print(result);
+    }
+
+}
