228b648be1b2103db05fde284a1ed862bc72c90c
Eunseo Kong <57488270+truthgarnet@users.noreply.github.com>
2025-06-25 22:58:27 +0900

    Title: 일렬로 서있는 소 2, Time: 145ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250625/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        int cnt = 0;
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i+1; j < n-1; j++) {
+                for (int z = j+1; z < n; z++) {
+                    if (arr[i] < arr[j] && arr[j] < arr[z]) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
