b5f18fcabaf1ea1100c6ab7388ef709efc5ef889
Eunseo Kong <57488270+truthgarnet@users.noreply.github.com>
2025-06-25 22:52:04 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250625/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i+1; j < str.length(); j++) {
+                if (str.charAt(i) == '(' && str.charAt(j) == ')') {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
