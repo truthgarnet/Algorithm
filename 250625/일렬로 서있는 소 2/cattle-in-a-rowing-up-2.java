1d6f5dbda534b66468443e0cf3de37605d349be5
Eunseo Kong <57488270+truthgarnet@users.noreply.github.com>
2025-06-25 22:59:57 +0900

    Title: 일렬로 서있는 소 2, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250625/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 250625/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -12,7 +12,7 @@ public class Main {
         for (int i = 0; i < n-2; i++) {
             for (int j = i+1; j < n-1; j++) {
                 for (int z = j+1; z < n; z++) {
-                    if (arr[i] < arr[j] && arr[j] < arr[z]) {
+                    if (arr[i] <= arr[j] && arr[j] <= arr[z]) {
                         cnt++;
                     }
                 }
