db78745cf6b99c03eceebcc9418b4c6da218305d
Eunseo Kong <57488270+truthgarnet@users.noreply.github.com>
2025-06-26 13:31:04 +0900

    Title: 만나는 그 순간, Time: 242ms, Memory: 38MB, Status: Passed - Codetree

--- 250626/만나는 그 순간/the-moment-we-meet.java
+++ 250626/만나는 그 순간/the-moment-we-meet.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    private static final int OFFSET = 1000 * 100 * 2;
+    private static final int OFFSET = 1000 * 1000 * 2;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
