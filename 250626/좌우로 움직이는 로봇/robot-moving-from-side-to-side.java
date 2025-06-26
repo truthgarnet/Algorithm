2806ddb43a7247c0f58aa57e33defd8fae5eefdd
Eunseo Kong <57488270+truthgarnet@users.noreply.github.com>
2025-06-26 14:36:49 +0900

    Title: 좌우로 움직이는 로봇, Time: 1201ms, Memory: 54MB, Status: Passed - Codetree

--- 250626/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250626/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,6 +9,7 @@ public class Main {
         int timeA = 1, timeB = 1;
         int[] arrA = new int[OFFSET];
         int[] arrB = new int[OFFSET];
+        boolean flag = false;
 
         for (int i = 0; i < n; i++) {
             int t = sc.nextInt();
@@ -18,7 +19,7 @@ public class Main {
                 if (d == 'L') 
                     arrA[timeA] = arrA[timeA-1] - 1; 
                 else 
-                    arrA[timeA] = arrA[timeA+1] + 1;
+                    arrA[timeA] = arrA[timeA-1] + 1;
                 timeA++;
             }
         }
@@ -31,18 +32,30 @@ public class Main {
                 if (d == 'L') 
                     arrB[timeB] = arrB[timeB-1] - 1; 
                 else 
-                    arrB[timeB] = arrB[timeB+1] + 1;
+                    arrB[timeB] = arrB[timeB-1] + 1;
                 timeB++;
             }
         }
         int cnt = 0;
         int maxTime = Math.max(timeA, timeB);
-        for (int i = 1; i < maxTime; i++) {
-            System.out.println(arrA[i] + " " + arrB[i]);
-            if (arrA[i] == arrB[i])
+        int minTime = Math.min(timeA, timeB);
+
+        for (int i = minTime; i < maxTime; i++) {
+            if (timeA > timeB) {
+                arrB[i] = arrB[timeB-1];
+            } else {
+                arrA[i] = arrA[timeA-1];
+            }
+        }
+        for (int i = 0; i < maxTime; i++) {
+            if ((arrA[i] == arrB[i]) && !flag) {
                 cnt++;
+                flag = true;
+            } else if (arrA[i] != arrB[i]) {
+                flag = false;
+            }
         }        
-        System.out.println(cnt);
+        System.out.println(cnt-1);
     }
     
 }
